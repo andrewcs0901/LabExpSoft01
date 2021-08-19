42fbda4d0e80381a23a135b69f0c870b33ed43f5
Andrew Costa <andrewcs0901@gmail.com>
2021-08-19 01:05:35 -0300

    feat: added query pagination

--- main.py
+++ main.py
@@ -1,58 +1,68 @@
 import csv
 from datetime import date, datetime
 import requests
+
 currentDate = date(2021, 8, 31)
-queries = ['main_query.txt', 'closed_issues.txt']
+txt_query = 'main_query.txt'
 token = open('env', 'r').read()
 
 
-def read_json_tocsv(res_json, str_acc=''):
+def format_data(key, value):
+    if key == 'createdAt' or key == 'updatedAt':
+        date_row = datetime.fromisoformat(value.replace('Z', '+00:00')).date()
+        return f"{(currentDate - date_row).days},"
+    return f"{value},"
+
+
+def read_json_to_csv(res_json, separator=''):
+    str_acc = ''
     for key in res_json:
         values = res_json[key]
-        if not isinstance(values, dict):
-            if key == 'createdAt' or key == 'updatedAt':
-                dateRow = datetime.fromisoformat(values.replace('Z', '+00:00')).date()
-                str_acc += f"{(currentDate - dateRow).days},"
-            else:
-                str_acc += f"{values},"
-        else:
-            for val in values.values():
-                if isinstance(val, dict):
-                    read_json_tocsv(val.values(), str_acc)
-                else:
-                    str_acc += f"{val},"
-    str_acc = f"{str_acc[:-1]}\n"
-    output.write(str_acc)
+        str_acc += format_data(key, values) \
+            if not isinstance(values, dict) \
+            else read_json_to_csv(values)
+    return f"{str_acc[:-1]}\n" if separator == '\n' else str_acc
 
 
 def read_json(res_json):
+    str_acc = ''
     for node in res_json:
-        read_json_tocsv(node)
-
-
-def create_csv_rows(json_row, str_acc=''):
-    for key in json_row.keys():
-        if not isinstance(json_row[key], dict):
-            str_acc += f"{key},"
-        else:
-            for key2 in json_row[key].keys():
-                if isinstance(json_row[key][key2], dict):
-                    str_acc += f"{key2}_"
-                    create_csv_rows(json_row[key][key2], str_acc)
-                else:
-                    str_acc += f"{key}_{key2},"
-    str_acc = f"{str_acc[:-1]}\n"
+        str_acc += read_json_to_csv(node, '\n')
     output.write(str_acc)
 
+
+def create_csv_rows(json_row, aux=''):
+    str_acc = aux
+    for key in json_row.keys():
+        str_acc += f"{key}," \
+            if not isinstance(json_row[key], dict) \
+            else create_csv_rows(json_row[key], f"{key}_")
+    return f"{str_acc[:-1]}\n" if len(aux) == 0 else str_acc
+
+
+def write_csv_rows(json_row):
+    output.write(create_csv_rows(json_row[0]))
+
+
 print('starting github graphql query...')
-for txt_query in queries:
-    query = open(txt_query, 'r').read()
-    output = open(f'result_{txt_query}.csv', 'w')
-    csvwriter = csv.writer(output)
-    headers = dict(Authorization=f'bearer {token}')
+query = open(txt_query, 'r').read()
+output = open(f'result_{txt_query}.csv', 'w')
+page = 0
+previous_cursor = 'null'
+headers = dict(Authorization=f'bearer {token}')
+while page < 10:
     r = requests.post('https://api.github.com/graphql', headers=headers, json={'query': query})
     if r.status_code == 200:
-        resJson = r.json()['data']['search']['nodes']
-        create_csv_rows(resJson[0])
-        read_json(resJson)
-print('Query executed')
+        print(f'page: {page}')
+        resJson = r.json()['data']['search']
+        nodes = resJson['nodes']
+        end_cursor = resJson['pageInfo']['endCursor']
+        if page == 0:
+            write_csv_rows(nodes)
+            query = query.replace(previous_cursor, f'"{end_cursor}"')
+        else:
+            query = query.replace(previous_cursor, end_cursor)
+        previous_cursor = end_cursor
+        read_json(nodes)
+        page += 1
+print('query executed')
